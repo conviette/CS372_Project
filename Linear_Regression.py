530d74d754e2184da6ca8395470a5582c452a930
sonhaksun10 <64411766+sonhaksun10@users.noreply.github.com>
2020-05-07 16:42:25 +0900

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -7,10 +7,12 @@ Max_Update = 100
 Epsilon = 0.0001
 Initial_Learning_Rate = 0.3
 
-def train_LR(d, label):
+def train_LR(d, label, lmda=0):
     '''
+
     :param data: data without bias
     :param label: label
+    :param lmda: regularization coef.
     :return:
     '''
     Learning_Rate = Initial_Learning_Rate
@@ -50,15 +52,15 @@ def train_LR(d, label):
     while(i<=Max_Update):
         diff = data @ theta - label
         square = diff.T @ diff
-        rms = square[0][0] / ex_size
+        mer = square[0][0] / ex_size
 
         if i>=1:
-            if mean_error_rate[-1] - rms < 0: #learning rate is too large that error rate increases
+            if mean_error_rate[-1] - mer < 0: #learning rate is too large that error rate increases
                 Learning_Rate /= 3.16 #multiple root(10)
                 i=0
                 print('decreased learning rate to ', Learning_Rate)
-            elif (mean_error_rate[-1] - rms) < Epsilon:
-                mean_error_rate.append(rms)
+            elif (mean_error_rate[-1] - mer) < Epsilon:
+                mean_error_rate.append(mer)
                 break
             if i==Max_Update: #learning rate is too small
                 Learning_Rate *= 3.16
@@ -67,15 +69,16 @@ def train_LR(d, label):
 
 
 
-        mean_error_rate.append(rms)
-        change_theta = Learning_Rate/ex_size*(data.T@diff)
+        mean_error_rate.append(mer)
+        change_theta = Learning_Rate/ex_size*(data.T@diff) + lmda*theta
         theta = theta - change_theta
         #print(theta)
         i+=1
 
-    print(theta)
-    print(mean_error_rate[-1])
-    return theta, normalize
+    #print(theta)
+    #print(mean_error_rate[-1])
+    accuracy = mean_error_rate[-1]
+    return theta, normalize, accuracy
 
 def test_LR(X,Y,theta,normalize):
     ex_size = len(X)
@@ -95,10 +98,60 @@ def test_LR(X,Y,theta,normalize):
     #test
     diff = data @ theta - Y
     square = diff.T @ diff
-    rms = square[0][0] / ex_size
-    print(rms)
+    mer = square[0][0] / ex_size
+    #print(mer)
 
-    return rms
+    return mer
+
+def k_fold(k,data,label):
+    def concat_all(data):  # concatinate all array in a list, all elements should have same number of columns
+        dat = data[0]
+        for ind in range(1, len(data)):
+            dat = np.concatenate((dat, data[ind]), axis=0)
+        return dat
+
+
+
+    folds_X = []
+    folds_Y = []
+    ex_size = len(data)
+    acc_train = []
+    acc_test = []
+
+    #shuffle data
+    idx = np.arange(ex_size)
+    np.random.shuffle(idx)
+    X = data[idx]
+    y = label[idx]
+
+    #divide folds
+    for i in range(k):
+        start_idx = i*ex_size//k
+        end_idx = (i+1)*ex_size//k
+        folds_X.append(X[start_idx:end_idx])
+        folds_Y.append(y[start_idx:end_idx])
+
+    for i in range(k):
+        X_train = folds_X[:]
+        X_test = X_train.pop(i)
+        y_train = folds_Y[:]
+        y_test = y_train.pop(i)
+        X_train = concat_all(X_train)
+        y_train = concat_all(y_train)
+
+        theta,normalize,accuracy_train = train_LR(X_train,y_train)
+        accuracy_test = test_LR(X_test,y_test,theta,normalize)
+        acc_train.append(accuracy_train)
+        acc_test.append(accuracy_test)
+
+    #print(acc_train)
+    #print(acc_test)
+
+
+    print("training accuracy", np.mean(acc_train))
+    print("test accuracy", np.mean(acc_test))
+
+    return np.mean(acc_train), np.mean(acc_test)
 
 
 
@@ -110,5 +163,10 @@ test_X = np.array([[0,0,0], [-1.2,-1.2,-1.5], [-3,-3,-3]])
 test_Y = np.array([[1,-0.3,-2]])
 test_Y = test_Y.T
 
-theta, normalize = train_LR(train_X, train_Y)
-test_LR(test_X,test_Y,theta,normalize)
+kf_X = np.array([[1,1,1],[2,2,2],[3,3,2],[4,4,5],[4,3,3],[1,2,1],[-1,-1,-1],[-2,-1,-2], [0,0,0], [-1.2,-1.2,-1.5], [-3,-3,-3]])
+kf_Y = np.array([[2,3,4,5,4,2,0,-1,1,-0.3,-2]])
+kf_Y = kf_Y.T
+
+k_fold(3,kf_X,kf_Y)
+#theta, normalize = train_LR(train_X, train_Y)
+#test_LR(test_X,test_Y,theta,normalize)
