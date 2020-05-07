b2a12e49c54621b93a6eb49e0e4f560a83770a9c
a-ntagonist <44609381+a-ntagonist@users.noreply.github.com>
2020-05-07 17:29:15 +0900

    connecting keyword and search

--- eco_rations.py
+++ eco_rations.py
@@ -1,18 +1,46 @@
-from nltk.corpus import inaugural
-from nltk.corpus import stopwords
+from nltk.corpus import inaugural, stopwords, wordnet
+from nltk.wsd import lesk
+
 
 eco_rations = dict()
 stop_words = set(stopwords.words('english'))
-for fileid in inaugural.fileids():
-    count = 0
-    nonstw = 0
-    for word in  inaugural.words(fileid):
-        for target in ["economy","employment","export","unemploy","export"]:
-            if not word.lower() in stop_words:
-                nonstw=nonstw+1
-                if word.lower().startswith(target):
-                    count=count+1
-    eco_rations[fileid[:4]] = count/nonstw
-print(eco_rations)
-                
 
+
+def syn_to_lem(key_synsets):
+    words = set()
+    for syns in key_synsets:
+        words.update(list(map(lambda x:x.name(), syns.lemmas())))
+    return words
+
+def clean_corpus(corpus):
+    return list(map(lambda sent:list(filter(lambda word:not word in stop_words, sent)), corpus))
+
+
+
+def main():
+    with open('economic_keywords.txt') as f:
+        keywords = f.readlines()
+    key_synsets = set(map(lambda x:wordnet.synset(x), keywords))
+    keywords = syn_to_lem(key_synsets)
+
+
+
+    for fileid in inaugural.fileids():
+        corpus = clean_corpus(inaugural.sents(fileid))
+        count = 0
+        total_len = sum(map(len, corpus))
+        for sent in corpus:
+            check = set(filter(lambda x:x in sent, keywords))
+            #check_synsets = set(map(lambda x:lesk(sent, x), check))
+            count += len(check)
+        eco_rations[fileid[:-4]] = count/total_len
+    print(eco_rations)
+
+    pres = list(eco_rations.keys())
+    pres.sort(key=lambda x:eco_rations[x])
+    print(pres)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ find_keywords.py
@@ -0,0 +1,13 @@
+from nltk.corpus import wordnet as wn
+
+seed = ['economy.n.01', 'trade.n.01', 'market.n.01']
+seed = list(map(lambda x:wn.synset(x), seed))
+lexicon = set(seed)
+
+for s in seed:
+    hypos = lambda x:x.hyponyms()
+    lexicon.update(set(s.closure(hypos)))
+
+with open('economic_keywords.txt', 'w') as f:
+    for l in lexicon:
+        f.write(l.name()+'\n')
