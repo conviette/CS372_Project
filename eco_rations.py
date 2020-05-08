452006bd54129c9836b58b25b228a609dc5bd248
sonhaksun10 <64411766+sonhaksun10@users.noreply.github.com>
2020-05-08 15:51:10 +0900

    Update eco_rations.py

--- eco_rations.py
+++ eco_rations.py
@@ -1,5 +1,6 @@
 from nltk.corpus import inaugural, stopwords, wordnet
 from nltk.wsd import lesk
+import numpy as np
 
 
 eco_rations = dict()
@@ -22,18 +23,27 @@ def main():
         keywords = f.readlines()
     key_synsets = set(map(lambda x:wordnet.synset(x), keywords))
     keywords = syn_to_lem(key_synsets)
-
+    key_synsets = list(key_synsets)
+    president_Synset_usage = dict() #The word the President said and its amount
 
 
     for fileid in inaugural.fileids():
         corpus = clean_corpus(inaugural.sents(fileid))
         count = 0
         total_len = sum(map(len, corpus))
+
+        Synset_usage = np.zeros((1,len(key_synsets)))
         for sent in corpus:
             check = set(filter(lambda x:x in sent, keywords))
             #check_synsets = set(map(lambda x:lesk(sent, x), check))
             count += len(check)
+            for w in check:
+                for i in range(len(key_synsets)):
+                    if w in key_synsets[i].lemma_names(): Synset_usage[0][i]+=1
+
+  
         eco_rations[fileid[:-4]] = count/total_len
+        president_Synset_usage[fileid[:-4]] = Synset_usage/total_len
     print(eco_rations)
 
     pres = list(eco_rations.keys())
