4fd7fde6ade995b7cd91d904f4eeeab798498137
sonhaksun10 <64411766+sonhaksun10@users.noreply.github.com>
2020-05-10 17:41:10 +0900

    Update find_keywords.py

--- find_keywords.py
+++ find_keywords.py
@@ -1,37 +1,23 @@
 from nltk.corpus import wordnet as wn
 import nltk
 
-def other_pos_word(syn_name):
-    # 
+def convert_and_similarity(syn_name):
     # Get all lemmas
-    word = syn_name.split('.')[0]
-    word_pos = syn_name.split('.')[1]
-    synsets = wn.synsets(word, pos=word_pos)
-    if (word_pos == 'a'): #both 'a' and 's' are adjective
-        synsets += wn.synsets(word, pos='s')
-    if not synsets:
-        return 0
-    lemmas = [l for s in synsets for l in s.lemmas()]
+    lemmas = [l for l in wn.synset(syn_name).lemmas()]
 
     # get related words
-    related = [related for l in lemmas for related in l.derivationally_related_forms()]
+    related = [r for l in lemmas for r in l.derivationally_related_forms()]
+    related += [r for l in lemmas for r in l.pertainyms()]
+
+    # Extract the synsets from the lemmas
+    new_synsets = [l.synset() for l in related]
+
+    return new_synsets
 
 
-    # Extract the words from the lemmas
-    words = [l.name() for l in related]
-    word_freq = nltk.FreqDist(words)
-    word_len = len(words)
-    word_usage = [(word_freq[w] / word_len, w) for w in set(words)]
-
-    #get synsets to get similarity
-    changed_words_synsets = [wn.synsets(s) for p,s in word_usage if p>0.1]
-    res = [s for syns in changed_words_synsets for s in syns]
-
-    return res
-
-
-seed = ['economy.n.01', 'trade.n.01', 'market.n.01', 'money.n.01', 'work.n.01', 'gold.n.05']
-other_pos = [other_pos_word(syn_name) for syn_name in seed]
+seed = ['economy.n.01', 'trade.n.01', 'market.n.01', 'money.n.01', 'work.n.01', 'worker.n.01']
+other_pos = [convert_and_similarity(syn_name) for syn_name in seed]
+print(other_pos)
 seed = list(map(lambda x:wn.synset(x), seed))
 seed += [s for syns in other_pos for s in syns]
 lexicon = set(seed)
