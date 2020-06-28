815ec393375527ef46861ebb6c9a1d1e995d7a80
a-ntagonist <44609381+a-ntagonist@users.noreply.github.com>
2020-06-28 17:08:01 +0900

    Update train_calc_features.py

--- Code/train_calc_features.py
+++ Code/train_calc_features.py
@@ -16,52 +16,28 @@ def clean_corpus(corpus): #any preprocessing for corpus
 #####functions for computing each features#########
 #####all functions must accept feature_dict as input, and add entry "feature_name" mapped to number
 
-def find_features_per_corpus(corpus_dict): #input corpus_dict: dict {year:[list of sents]}
-    features_for_year = dict()
-    keywords = set()
-    for fileid in corpus_dict: #for each year
-        features_for_year[fileid] = dict()
-        corpus = clean_corpus(corpus_dict[fileid]) #clean corpus
-        vocabulary_unigram = FreqDist([word for sent in corpus for word in sent])
-        unigram_pdist = DictionaryProbDist(vocabulary_unigram, normalize=True)
-        unigram_pdist = dict((x[0], unigram_pdist.prob(x[0])) for x in vocabulary_unigram.most_common(100))
-        for key in unigram_pdist:
-            keywords.add(key)
-        features_for_year[fileid].update(unigram_pdist)
-        big_corpus = [(s[i], s[i+1]) for s in corpus for i in range(len(s)-1)]
-        vocab_bigram = FreqDist(big_corpus)
-        bigram_pdist = DictionaryProbDist(vocab_bigram, normalize=True)
-        bigram_pdist = dict(('_'.join(x[0]), bigram_pdist.prob(x[0])) for x in vocab_bigram.most_common(30))
-        for key in bigram_pdist:
-            keywords.add(key)
-        features_for_year[fileid].update(bigram_pdist)
-        #add bigrams
 
-
-    for d in features_for_year.values():
-        for key in keywords:
-            if not key in d:
-                d[key] = 0.0
-
-    return features_for_year #output features_for_year: dict {year:{feature_name:float}}
-
-def find_features_agg(corpus_dict):
+def find_features_agg(corpus_dict): ###edit this function to add featuress
     features_for_year = dict()
     keywords = set()
     supercorpus = []
     for fileid in corpus_dict: #for each year
         features_for_year[fileid] = dict()
-        supercorpus.extend(clean_corpus(corpus_dict[fileid])) #clean corpus
+        supercorpus.extend(clean_corpus(corpus_dict[fileid])) #concatenated corpus (all)
+
+    ##list of possible features (ex for unigrams, list of 1500 most common unigrams in all of corpus)
     super_unigram = [x[0] for x in FreqDist([word for sent in supercorpus for word in sent]).most_common(1500)]
     super_bigram = ['_'.join(x[0]) for x in FreqDist([(s[i], s[i+1]) for s in supercorpus for i in range(len(s)-1)]).most_common(500)]
 
     for fileid in corpus_dict:
         corpus = clean_corpus(corpus_dict[fileid])
+
+        ####computing feature for EACH document###
         vocabulary_unigram = FreqDist([word for sent in corpus for word in sent])
         unigram_pdist = DictionaryProbDist(vocabulary_unigram, normalize=True)
         for key in super_unigram:
-            if key in vocabulary_unigram:
-                features_for_year[fileid][key] = unigram_pdist.prob(key)
+            if key in vocabulary_unigram: ##fileid = document id, key=unigram
+                features_for_year[fileid][key] = unigram_pdist.prob(key) ###normalized frequency of unigram for document
             else:
                 features_for_year[fileid][key] = 0.0
 
@@ -73,6 +49,10 @@ def find_features_agg(corpus_dict):
                 features_for_year[fileid][key] = bigram_pdist.prob(key)
             else:
                 features_for_year[fileid][key] = 0.0
+
+        ####add feature here#####
+        ####save frequency to features_for_year[fileid][featurename]
+        ####all documents should have same features (no features that exist in only some of the documents)
 
     return features_for_year #output features_for_year: dict {year:{feature_name:float}}
 
